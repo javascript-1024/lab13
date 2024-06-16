3c0c551f3daa2a998ab302f58db143e9ddcbd4bd
Trương Tiến Đạt <94352543+TienDat115@users.noreply.github.com>
2024-06-16 20:57:45 +0700

    update

--- script.js
+++ script.js
@@ -1,148 +1,23 @@
+import videoLinks from "./videoLinks.js"; // Adjust the path as necessary
+
 document.addEventListener("DOMContentLoaded", function () {
 	const iframe = document.getElementById("videoPlayer");
 	const episodeSelect = document.getElementById("episodeSelect");
 	const filmSelect = document.getElementById("filmSelect");
-	const note = document.getElementById("note");
 
 	const timeInput = document.getElementById("timeInput");
 	const convertButton = document.getElementById("convertButton");
 	const timeInSeconds = document.getElementById("timeInSeconds");
-
-	const videoLinks = {
-		film1: {
-			1: "https://embed3.streamc.xyz/embed.php?hash=60da6a8137cb20f06ec036ab829b79d5",
-			2: "https://embed3.streamc.xyz/embed.php?hash=b8aa76a31460ffb790bd862f1655f729",
-			3: "https://embed3.streamc.xyz/embed.php?hash=55366e9191271ddc3a81021d70a2771d",
-			4: "https://embed3.streamc.xyz/embed.php?hash=02320975f518c0076c9a6c1d538aff52",
-			5: "https://embed2.streamc.xyz/embed.php?hash=530cabd6b67a9a0f5d54f441e3d0ddcf",
-			6: "https://embed2.streamc.xyz/embed.php?hash=f9149c02f02e65d1eb37c9334d223d66",
-			7: "https://embed2.streamc.xyz/embed.php?hash=a3861e87acc81089bf611b5664bd7543",
-			8: "https://embed2.streamc.xyz/embed.php?hash=22e22ded4ce034fdf6797684339d3ab8",
-			9: "https://embed3.streamc.xyz/embed.php?hash=1187d6ba4ccc18f5a83cc4bb5659f48a",
-			10: "https://embed3.streamc.xyz/embed.php?hash=f7603fbce594df2c52d5263d62378689",
-			11: "https://embed3.streamc.xyz/embed.php?hash=2f00fc693a8b99f6363fdae02b849d55",
-			12: "https://embed3.streamc.xyz/embed.php?hash=1fc4f2bc956f62cdd0c98f90293252eb",
-			13: "https://embed3.streamc.xyz/embed.php?hash=79c53dd7b6ca3141409c567d4f533960",
-			14: "https://embed3.streamc.xyz/embed.php?hash=f1eae3cae91e4801eb46dc04418390b5",
-			15: "https://embed2.streamc.xyz/embed.php?hash=581168ac48abb9077194a79553b96b79",
-			16: "https://embed2.streamc.xyz/embed.php?hash=771fdfc7c8b7a74fbdd74e3192121dd0",
-			17: "https://embed2.streamc.xyz/embed.php?hash=2382230a986d717632c44812493eecde",
-			18: "https://embed2.streamc.xyz/embed.php?hash=67a7654626270af5d9a56f61d10c1bf0",
-			19: "https://embed2.streamc.xyz/embed.php?hash=37b82524e829e8e8ab4168f785b3243c",
-			20: "https://embed2.streamc.xyz/embed.php?hash=a5c58e83238ef410a5d740a199868ae8",
-			21: "https://embed2.streamc.xyz/embed.php?hash=488e6e1bb8799143d2fb90e24575d1e2",
-			22: "https://embed2.streamc.xyz/embed.php?hash=0695da96227c55810d33ecca8439e76b",
-			23: "https://embed2.streamc.xyz/embed.php?hash=72bd4e60a9ae50035239063f58559d51",
-			24: "https://embed2.streamc.xyz/embed.php?hash=f43d9d18dc7d1ad74fd260372a65575a",
-			25: "https://embed2.streamc.xyz/embed.php?hash=7736587e88dd8d7d5b3268533bbf7ee2",
-			26: "https://embed2.streamc.xyz/embed.php?hash=6c5c5a8697abe91b03be7130420d022a",
-			27: "https://embed2.streamc.xyz/embed.php?hash=cd86f332332281b638cc2f7cb30e3cca",
-			28: "https://embed2.streamc.xyz/embed.php?hash=6b72a5335eb0ab539786d954ea42f390",
-			29: "https://embed2.streamc.xyz/embed.php?hash=4c4916eb12a4b53237890d7e788194d1",
-			30: "https://embed2.streamc.xyz/embed.php?hash=ec7920e3103c04e0634bbad2e3c3d411",
-			31: "https://embed2.streamc.xyz/embed.php?hash=2f38ac72f25c744befe25d7c6ef85b6f",
-			32: "https://embed2.streamc.xyz/embed.php?hash=7503984a78f73139f3486c7b9d345b50",
-			33: "https://embed2.streamc.xyz/embed.php?hash=41ed647c2215d851312abc870fdc3111",
-			34: "https://embed2.streamc.xyz/embed.php?hash=7c182283988835d4148c0badc1033397",
-			35: "https://embed2.streamc.xyz/embed.php?hash=ae23feff45e38b11ed1d6cb23b61c928",
-			36: "https://embed2.streamc.xyz/embed.php?hash=ce3ad183e8d4cf2e0181a5ac7a78421d",
-		},
-		film2: {
-			1: "https://embed3.streamc.xyz/embed.php?hash=07431a12712e36906e1faac3ac05ae10",
-			2: "https://embed3.streamc.xyz/embed.php?hash=d1f8c9f34876e772d7a3ab618edc33f2",
-			3: "https://embed2.streamc.xyz/embed.php?hash=b22af338e60fd4ea27a0dcd3d7a0bfff",
-			4: "https://haivan.info/player/?d=4n8bAPg&v=p&mad=1&ref=https://motchillj.net/xem-phim-pha-ken-2-tap-4-147208.html",
-			5: "https://embed2.streamc.xyz/embed.php?hash=dda3c1346514e36d5630368441b889d6",
-			6: "https://embed2.streamc.xyz/embed.php?hash=92d2f1a8b5085a0982534d8d3d01ce1d",
-			7: "https://embed2.streamc.xyz/embed.php?hash=faaf42be3bdcd16765ed4ae80b3eee18",
-			8: "https://embed2.streamc.xyz/embed.php?hash=72744e9f873a132519b9e81d61884389",
-			9: "https://embed2.streamc.xyz/embed.php?hash=173f6ebab883329bd2a37879634892e9",
-			10: "https://embed3.streamc.xyz/embed.php?hash=02e6990900d1ebf536b8ef5ff15bfbdd",
-			11: "https://embed2.streamc.xyz/embed.php?hash=e4c875888f89a9533fe1c7d1e7c52cfc",
-			12: "https://embed3.streamc.xyz/embed.php?hash=64f55eeb62498e57f05d1a54a421ffbc",
-			13: "https://embed2.streamc.xyz/embed.php?hash=3cb269d1d50502ee4295198d5394413f",
-			14: "https://embed2.streamc.xyz/embed.php?hash=b8fcbc525477d42ec9d1ea6c563374db",
-			15: "https://embed3.streamc.xyz/embed.php?hash=5176402b8bddd710b5a123460d79618f",
-			16: "https://embed3.streamc.xyz/embed.php?hash=aa33603a84acae69c69727129ebc86c2",
-			17: "https://embed3.streamc.xyz/embed.php?hash=9427745e8dfd25c11d2cf344ddfdb4f6",
-			18: "https://embed2.streamc.xyz/embed.php?hash=c0df6bec65dd1c9fecb04899a97c1ce7",
-			19: "https://embed2.streamc.xyz/embed.php?hash=84514f96c587907b0383a5be1f2fc61c",
-			20: "https://embed2.streamc.xyz/embed.php?hash=38e780138091264bc7a399eb79acf038",
-			21: "https://embed2.streamc.xyz/embed.php?hash=f8558b218789e286561ad7f0d427d9e7",
-			22: "https://embed3.streamc.xyz/embed.php?hash=dfbce5782fa7cec726564aa75f33d86d",
-			23: "https://haivan.info/player/?d=LWnJ1vq&v=p&mad=1&ref=https://motchillj.net/xem-phim-pha-ken-2-tap-23-147574.html",
-			24: "https://haivan.info/player/?d=A4xag0Q&v=p&mad=1&ref=https://motchillj.net/xem-phim-pha-ken-2-tap-24-147575.html",
-			25: "https://haivan.info/player/?d=P7gy2R5&v=p&mad=1&ref=https://motchillj.net/xem-phim-pha-ken-2-tap-25-147706.html",
-			26: "https://haivan.info/player/?d=4gyvopM&v=p&mad=1&ref=https://motchillj.net/xem-phim-pha-ken-2-tap-26-147707.html",
-		},
-		film3: {
-			VietSub: "https://embed.streamc.xyz/embed.php?hash=348ca95ddd3f70939276bafd8ebf0338",
-			"Lồng Tiếng": "https://embed.streamc.xyz/embed.php?hash=b3b67f07cd8ed6dbd8b35d326685aa04",
-		},
-		film4: {
-			1: "https://embed2.streamc.xyz/embed.php?hash=b1a32e6ebf296a2bb5e1e2d4018cbece",
-			2: "https://embed2.streamc.xyz/embed.php?hash=b2e2b956f7b8e76290b5e0cf3444ccd3",
-			3: "https://embed2.streamc.xyz/embed.php?hash=f5ce39f362b102b0b80c5c3ff0540711",
-			4: "https://embed2.streamc.xyz/embed.php?hash=51a803fa6201bb9ac6e0bc595d873b4b",
-			5: "https://embed2.streamc.xyz/embed.php?hash=0256eda0ac193efd4badcc2728e0d234",
-			6: "https://embed2.streamc.xyz/embed.php?hash=25c8b3233c642272de7bcfb3aeb255bf",
-			7: "https://embed2.streamc.xyz/embed.php?hash=0627f51354800dc565ca6e6ccc7912b0",
-			8: "https://embed2.streamc.xyz/embed.php?hash=b55f373300f98a9ab8e02d4752c47001",
-			9: "https://embed2.streamc.xyz/embed.php?hash=52a88c6d171f7d62f631ba21d58b88f9",
-			10: "https://embed2.streamc.xyz/embed.php?hash=20a0f1b1b0e5cd5fb1f4a91c1ff2bd5a",
-			11: "https://embed2.streamc.xyz/embed.php?hash=d0e7bb2c9f3940de09278fe316c51aaf",
-			12: "https://embed2.streamc.xyz/embed.php?hash=90c35086fc9838aa363a613299373674",
-			13: "https://embed2.streamc.xyz/embed.php?hash=9f8efe392dcd0d9823f549e246c985c0",
-			14: "https://embed2.streamc.xyz/embed.php?hash=3fd990c71e8a15d26505af66d313e7f1",
-			15: "https://embed2.streamc.xyz/embed.php?hash=e88b1de7a82f492738a5d94be64e86d6",
-			16: "https://embed2.streamc.xyz/embed.php?hash=089f459923f267d4bcd743c7604b21cf",
-			17: "https://embed2.streamc.xyz/embed.php?hash=d6bf45e60a76966744f0d6572347054c",
-			18: "https://embed2.streamc.xyz/embed.php?hash=d64b94740d2fe3fdf444837edd8c867d",
-			19: "https://embed2.streamc.xyz/embed.php?hash=b2fda4273d9ba8bf941d74dd746ef24e",
-			20: "https://embed2.streamc.xyz/embed.php?hash=f05bfb0417458d1396e3f4e57b50eff0",
-			21: "https://embed2.streamc.xyz/embed.php?hash=cb19fda75305b532f1f48708bf6fb960",
-			22: "https://embed2.streamc.xyz/embed.php?hash=4a83b434a8fa3661a62fa9871233c2e9",
-			23: "https://embed2.streamc.xyz/embed.php?hash=c0e85728e153e9422c1e3da94219cfe1",
-			24: "https://embed2.streamc.xyz/embed.php?hash=ff89dbe766cb0752f7a70e964b4a758b",
-			25: "https://embed2.streamc.xyz/embed.php?hash=97569e8e4f452745ada720454f3b25d5",
-			26: "https://embed2.streamc.xyz/embed.php?hash=82817aad5654a21c91fe594703ca4a10",
-			27: "https://embed2.streamc.xyz/embed.php?hash=b28b9a611e8025da750a25c14a2f35ff",
-			28: "https://embed2.streamc.xyz/embed.php?hash=a4aa554a00ac5126f36a9a21d9448a33",
-			29: "https://embed2.streamc.xyz/embed.php?hash=38fb956ecd05096f80f2de8195f51c0d",
-			30: "https://embed2.streamc.xyz/embed.php?hash=d5a582e19ef4f09b88e1c1416bbe6701",
-			31: "https://embed2.streamc.xyz/embed.php?hash=086e68608cada3c31edd5853b578481c",
-			32: "https://embed2.streamc.xyz/embed.php?hash=0ffa22e60539f78a58cb5a26b956863e",
-			33: "https://embed2.streamc.xyz/embed.php?hash=f3e03f874a88b55b93627747840ffac8",
-			34: "https://embed2.streamc.xyz/embed.php?hash=eeac8e6d2d2b9f36954f479111efb9a9",
-			35: "https://embed2.streamc.xyz/embed.php?hash=97d86be95c39d765c9998f795a7592d1",
-			36: "https://embed2.streamc.xyz/embed.php?hash=3f418e51d156e5915cd5a3b21989df13",
-			37: "https://embed2.streamc.xyz/embed.php?hash=376af575f78d6c1f5f7688d0c82c4749",
-			38: "https://embed2.streamc.xyz/embed.php?hash=805ad90feb8a030374c4b15fd4f96c20",
-			39: "https://embed2.streamc.xyz/embed.php?hash=0b6a446e82d644ebef575cbbb55561e6",
-			40: "https://embed2.streamc.xyz/embed.php?hash=c37043486807c2f0b2afdc5fb203c501",
-			41: "https://embed2.streamc.xyz/embed.php?hash=36b498b590f08bcde1d64e0a42e8a0f1",
-			42: "https://embed2.streamc.xyz/embed.php?hash=c8ae21f7d41c4891c97a8e06716d744e",
-			43: "https://embed2.streamc.xyz/embed.php?hash=6fe33b785c6fdc04c8c1ec63e067de92",
-			44: "https://embed2.streamc.xyz/embed.php?hash=548123624e080ee71bdde0cc5d1f758d",
-			45: "https://embed2.streamc.xyz/embed.php?hash=2fee3799a636e59855c4dd4e2cd9d3d7",
-			46: "https://embed2.streamc.xyz/embed.php?hash=1f32e0bf1928d0218566fb64065e0d09",
-		},
-		film5: {
-			"Lồng Tiếng": "https://embed3.streamc.xyz/embed.php?hash=0b98da3a006f8f5c95d875a3494e33fc",
-		},
-	};
 
 	function updateFilmOptions() {
 		const filmTypeSelect = document.getElementById("filmTypeSelect");
 		const filmSelect = document.getElementById("filmSelect");
 		const selectedType = filmTypeSelect.value;
 
-		// Xóa tất cả các tùy chọn hiện tại
 		while (filmSelect.firstChild) {
 			filmSelect.removeChild(filmSelect.firstChild);
 		}
 
-		// Tạo các tùy chọn phim mới dựa trên loại phim được chọn
 		let options = [];
 		if (selectedType === "phimLe") {
 			options = [
@@ -157,7 +32,6 @@ document.addEventListener("DOMContentLoaded", function () {
 			];
 		}
 
-		// Thêm các tùy chọn mới vào thẻ <select>
 		options.forEach((option) => {
 			const opt = document.createElement("option");
 			opt.value = option.value;
@@ -172,21 +46,13 @@ document.addEventListener("DOMContentLoaded", function () {
 	function loadEpisode(film) {
 		const currentEpisode = localStorage.getItem(film) || 1;
 		const videoUrl = videoLinks[film][currentEpisode];
-		iframe.src = videoUrl;
-		localStorage.setItem("currentFilm", film);
-		episodeSelect.value = currentEpisode;
-	}
-
-	function loadNote() {
-		const film = filmSelect.value;
-		const currentNote = localStorage.getItem(`${film}Note`) || "";
-		note.value = currentNote;
-	}
-
-	function saveNote() {
-		const film = filmSelect.value;
-		const currentNote = note.value;
-		localStorage.setItem(`${film}Note`, currentNote);
+		if (videoUrl) {
+			iframe.src = videoUrl;
+			localStorage.setItem("currentFilm", film);
+			episodeSelect.value = currentEpisode;
+		} else {
+			console.error(`Video URL for ${film} episode ${currentEpisode} is not available.`);
+		}
 	}
 
 	function saveTimeInput() {
@@ -209,11 +75,7 @@ document.addEventListener("DOMContentLoaded", function () {
 		episodes.forEach((episode) => {
 			let option = document.createElement("option");
 			option.value = episode;
-			if (selectedType === "phimLe") {
-				option.textContent = `${episode}`;
-			} else if (selectedType === "phimBo") {
-				option.textContent = `Tập ${episode}`;
-			}
+			option.textContent = selectedType === "phimLe" ? episode : `Tập ${episode}`;
 			episodeSelect.appendChild(option);
 		});
 	}
@@ -226,8 +88,12 @@ document.addEventListener("DOMContentLoaded", function () {
 		const currentEpisode = parseInt(episodeSelect.value);
 		if (currentEpisode > 1) {
 			const previousEpisode = currentEpisode - 1;
-			localStorage.setItem(selectedFilm, previousEpisode);
-			loadEpisode(selectedFilm);
+			if (videoLinks[selectedFilm][previousEpisode]) {
+				localStorage.setItem(selectedFilm, previousEpisode);
+				loadEpisode(selectedFilm);
+			} else {
+				console.error(`Video URL for ${selectedFilm} episode ${previousEpisode} is not available.`);
+			}
 		}
 	});
 
@@ -237,8 +103,12 @@ document.addEventListener("DOMContentLoaded", function () {
 		const totalEpisodes = Object.keys(videoLinks[selectedFilm]).length;
 		if (currentEpisode < totalEpisodes) {
 			const nextEpisode = currentEpisode + 1;
-			localStorage.setItem(selectedFilm, nextEpisode);
-			loadEpisode(selectedFilm);
+			if (videoLinks[selectedFilm][nextEpisode]) {
+				localStorage.setItem(selectedFilm, nextEpisode);
+				loadEpisode(selectedFilm);
+			} else {
+				console.error(`Video URL for ${selectedFilm} episode ${nextEpisode} is not available.`);
+			}
 		}
 	});
 
@@ -246,7 +116,6 @@ document.addEventListener("DOMContentLoaded", function () {
 		const selectedFilm = filmSelect.value;
 		populateEpisodeSelect(selectedFilm);
 		loadEpisode(selectedFilm);
-		// loadNote();
 	});
 
 	episodeSelect.addEventListener("change", function () {
@@ -256,23 +125,20 @@ document.addEventListener("DOMContentLoaded", function () {
 		loadEpisode(selectedFilm);
 	});
 
-	// note.addEventListener("input", saveNote);
-
 	convertButton.addEventListener("click", function () {
 		const timeParts = timeInput.value.split(":");
 		const minutes = parseInt(timeParts[0], 10) || 0;
 		const seconds = parseInt(timeParts[1], 10) || 0;
 		const totalSeconds = minutes * 60 + seconds;
 		timeInSeconds.textContent = `Thời gian tính bằng giây: ${totalSeconds}`;
-		saveTimeInput(); // Save time input value
+		saveTimeInput();
 	});
 
 	const currentFilm = localStorage.getItem("currentFilm") || "film1";
 	filmSelect.value = currentFilm;
 	populateEpisodeSelect(currentFilm);
 	loadEpisode(currentFilm);
-	// loadNote();
-	loadTimeInput(); // Load time input value when page loads
+	loadTimeInput();
 
 	document.getElementById("filmTypeSelect").addEventListener("change", updateFilmOptions);
 	updateFilmOptions();
